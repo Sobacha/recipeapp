fe453159764a6b8281adc79b6b3c0c5ea6a039c1
Rima <rmhrok@gmail.com>
2017-05-23 16:48:13 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   resources :recipes
 
   resources :foods, except: :show
+
+  resources :users
 
 #  get 'recipes/search/:id', to: 'recipes#search'
   get 'recipes/search/:name', to: 'recipes#search'
