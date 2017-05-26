9a232ba732f40baad9eaed84810f762dd524e30a
Rima <rmhrok@gmail.com>
2017-05-26 14:43:37 -0700

    Implement basic login

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  validates :category, presence: true, length: { maximum: 50}
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :ingredients, presence: true
+  validates :direction, presence: true
   validates :user_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'signup', to: 'users#new'
-  post '/signup',  to: 'users#create'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 
   resources :recipes
-
   resources :foods, except: :show
-
   resources :users
 
-#  get 'recipes/search/:id', to: 'recipes#search'
   get 'recipes/search/:name', to: 'recipes#search'
 
   root 'welcome#index'
