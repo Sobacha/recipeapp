d3d4321a65eb0ddcbea59f60031f56f2daff568a
Rima <rmhrok@gmail.com>
2017-06-04 07:24:31 -0700

    Passed sessions controller routes test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
+  # get 'sessions/new'
   get 'signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   get 'recipes/search/:name', to: 'recipes#search'
 
   root 'welcome#home'
-  
+
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -7,6 +7,13 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  
+  test "sessions routings" do
+    assert_routing({method: 'get', path: 'login'},
+                   {controller: "sessions", action: "new"})
+    assert_routing({method: 'post', path: 'login'},
+                   {controller: "sessions", action: "create"})
+    assert_routing({method: 'delete', path: 'logout'},
+                   {controller: "sessions", action: "destroy"})
+  end
 
 end
