ff7cc30dbd73d135f03faff8ebbbd72af84be25b
Rima <rmhrok@gmail.com>
2017-06-09 13:39:56 -0700

    Passed recipes controller route test

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "recipes routings" do
+    assert_routing({method: 'get', path: 'recipes'},
+                   {controller: "recipes", action: "index"})
+    assert_routing({method: 'get', path: 'recipes/1'},
+                   {controller: "recipes", action: "show", id: "1"})
+    assert_routing({method: 'get', path: 'recipes/new'},
+                   {controller: "recipes", action: "new"})
+    assert_routing({method: 'post', path: 'recipes'},
+                   {controller: "recipes", action: "create"})
+    assert_routing({method: 'get', path: 'recipes/1/edit'},
+                   {controller: "recipes", action: "edit", id: "1"})
+    assert_routing({method: 'patch', path: 'recipes/1'},
+                   {controller: "recipes", action: "update", id: "1"})
+    # assert_routing({method: 'put', path: 'recipes/1'},
+    #                {controller: "recipes", action: "update", id: "1"})
+    assert_routing({method: 'delete', path: 'recipes/1'},
+                   {controller: "recipes", action: "destroy", id: "1"})
+  end
+
 end
