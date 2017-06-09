b334b02fad8d30c95755bbb3b54f7e5933660277
Rima <rmhrok@gmail.com>
2017-06-09 13:59:08 -0700

    Passed foods controller route test

--- test/controllers/foods_controller_test.rb
+++ test/controllers/foods_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class FoodsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "foods routings" do
+    assert_routing({method: 'get', path: 'foods'},
+                   {controller: "foods", action: "index"})
+    assert_routing({method: 'get', path: 'foods/new'},
+                   {controller: "foods", action: "new"})
+    assert_routing({method: 'post', path: 'foods'},
+                   {controller: "foods", action: "create"})
+    assert_routing({method: 'get', path: 'foods/1/edit'},
+                   {controller: "foods", action: "edit", id: "1"})
+    assert_routing({method: 'patch', path: 'foods/1'},
+                   {controller: "foods", action: "update", id: "1"})
+    # assert_routing({method: 'put', path: 'foods/1'},
+    #                {controller: "foods", action: "update", id: "1"})
+    assert_routing({method: 'delete', path: 'foods/1'},
+                   {controller: "foods", action: "destroy", id: "1"})
+  end
+
 end
