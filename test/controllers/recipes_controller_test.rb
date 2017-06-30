f5cc3fa0509882d11db79a6d997d1b3bc240cba9
Rima <rmhrok@gmail.com>
2017-06-30 13:49:15 -0700

    Passed invalid and valid edit recipe test

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -23,6 +23,7 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:one)
+    @recipe = recipes(:two)
   end
 
   test "Invalid recipe - No title(Empty input)" do
@@ -132,6 +133,117 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
                                              url: "google.com",
                                              user_id: @user.id } }
     end
+    follow_redirect!
+    assert_template 'show'
+  end
+
+  test "Invalid edit recipe - No title(Empty input) - (from the list of recipes page)" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+
+    get recipes_path
+    get edit_recipe_path(@recipe)
+    # how to test text field is prefilled?
+    patch recipe_path(@recipe), params: { recipe: { category: @recipe.category,
+                                                    title: "",
+                                                    ingredients: @recipe.ingredients,
+                                                    direction: @recipe.direction,
+                                                    url: @recipe.url,
+                                                    user_id: @recipe.user_id } }
+    assert_template 'edit'
+    assert_select "div.alert", text: "1 error prohibited this recipe from being saved:"
+  end
+
+  test "Invalid edit recipe - No title(Only space input) - (from the recipe show page)" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+
+    get recipes_path
+    get recipe_path(@recipe)
+    get edit_recipe_path(@recipe)
+    # how to test text field is prefilled?
+    patch recipe_path(@recipe), params: { recipe: { category: @recipe.category,
+                                                    title: "        ",
+                                                    ingredients: @recipe.ingredients,
+                                                    direction: @recipe.direction,
+                                                    url: @recipe.url,
+                                                    user_id: @recipe.user_id } }
+    assert_template 'edit'
+    assert_select "div.alert", text: "1 error prohibited this recipe from being saved:"
+  end
+
+  test "Invalid edit recipe - Title is too long" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+
+    get recipes_path
+    get edit_recipe_path(@recipe)
+    # how to test text field is prefilled?
+    patch recipe_path(@recipe), params: { recipe: { category: @recipe.category,
+                                                    title: 'a'*51,
+                                                    ingredients: @recipe.ingredients,
+                                                    direction: @recipe.direction,
+                                                    url: @recipe.url,
+                                                    user_id: @recipe.user_id } }
+    assert_template 'edit'
+    assert_select "div.alert", text: "1 error prohibited this recipe from being saved:"
+  end
+
+  test "Invalid edit recipe - Category is too long" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+
+    get recipes_path
+    get edit_recipe_path(@recipe)
+    # how to test text field is prefilled?
+    patch recipe_path(@recipe), params: { recipe: { category: 'a'*51,
+                                                    title: @recipe.title,
+                                                    ingredients: @recipe.ingredients,
+                                                    direction: @recipe.direction,
+                                                    url: @recipe.url,
+                                                    user_id: @recipe.user_id } }
+    assert_template 'edit'
+    assert_select "div.alert", text: "1 error prohibited this recipe from being saved:"
+  end
+
+  test "Valid edit recipe" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+
+    get recipes_path
+    get edit_recipe_path(@recipe)
+    # how to test text field is prefilled?
+    patch recipe_path(@recipe), params: { recipe: { category: "Fruit salad",
+                                                    title: "Fruit salad",
+                                                    ingredients: @recipe.ingredients,
+                                                    direction: @recipe.direction,
+                                                    url: @recipe.url,
+                                                    user_id: @recipe.user_id } }
     follow_redirect!
     assert_template 'show'
   end
